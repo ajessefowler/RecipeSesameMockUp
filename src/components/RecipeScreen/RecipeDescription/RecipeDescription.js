fc2cf0e98c520c6f0d207745f6fa3471e55673fa
Jesse Fowler <ajessefowler@gmail.com>
2022-02-11 14:55:23 -0500

    Replaced paragraph font with something more readable. Added space and border between ingredients and instructions.

--- src/components/RecipeScreen/RecipeDescription/RecipeDescription.js
+++ src/components/RecipeScreen/RecipeDescription/RecipeDescription.js
@@ -2,11 +2,10 @@ import React from "react";
 import './RecipeDescription.css';
 
 const RecipeDescription = (props) => {
-    //{ props.description.map(line => <p>{line}</p>) }
     return (
         <div className="recipeDescription">
             { props.recipe.ingredients.map(line => <p>{line}</p>) }
-            <p>{ props.recipe.instructions }</p>
+            <p className="recipeInstructions">{ props.recipe.instructions }</p>
         </div>
     );
 }
