0e5072dd952981af863d57da83b372b6038b7d08
Jesse Fowler <ajessefowler@gmail.com>
2022-02-11 15:38:38 -0500

    Now gets 3 random recipes on initial load. Also made some font and design changes, but don't really like the smaller font...

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import RecommendedContainer from './components/HomeScreen/RecommendedContainer/R
 import CartButton from './components/HomeScreen/CartButton/CartButton';
 import RecipeContainer from './components/RecipeScreen/RecipeContainer/RecipeContainer';
 import CartContainer from './components/CartScreen/CartContainer/CartContainer';
+import { useEffect } from 'react/cjs/react.development';
 
 function App() {
   const [numberOfMessagesSent, setNumberOfMessagesSent] = useState(0);
@@ -18,6 +19,14 @@ function App() {
   const [cartScreenIsOpen, setCartScreenIsOpen] = useState(false);
   const [cartContents, setCartContents] = useState(["1.5 lbs chicken tenders", "4 tbsp extra virgin olive oil", "1 cup Greek yogurt", "1 large lemon", "2 tbsp red wine vinegar", "3 garlic gloves", "kosher salt", "black pepper", "1 tbsp oregano", "1 tsp sweet paprika", "1 tsp ground cumin", "1 tsp ground coriander"]);
 
+  const randomUrl = 'http://localhost:8000/random/';
+
+  useEffect(() => {
+    fetch(randomUrl)
+      .then(response => response.json())
+      .then(data => setRecommendedRecipes(data));
+  }, [])
+
   return (
     <div className="App">
       <Logo />
@@ -25,17 +34,22 @@ function App() {
         setCartScreenIsOpen={setCartScreenIsOpen}
       />
       <div className="mainContainer">
-        <Chatbox 
-          numberOfMessagesSent={numberOfMessagesSent} 
-          setNumberOfMessagesSent={setNumberOfMessagesSent}
-          setRecommendedRecipes={setRecommendedRecipes}
-        />
+        <div className="chatContainer">
+          <h3>Tell us what you're looking for!</h3>
+          <Chatbox 
+            numberOfMessagesSent={numberOfMessagesSent} 
+            setNumberOfMessagesSent={setNumberOfMessagesSent}
+            setRecommendedRecipes={setRecommendedRecipes}
+          />
+        </div>
+        
         <RecommendedContainer
           setCartContents={setCartContents} 
           setOpenRecipe={setOpenRecipe}
           setRecipeScreenIsOpen={setRecipeScreenIsOpen}
           numberOfMessagesSent={numberOfMessagesSent} 
           recommendedRecipes={recommendedRecipes}
+          setRecommendedRecipes={setRecommendedRecipes}
         />
       </div>
 
--- src/components/HomeScreen/Logo/Logo.js
+++ src/components/HomeScreen/Logo/Logo.js
@@ -5,7 +5,6 @@ const Logo = () => {
     return (
         <div className="logo">
             <h1>Recipe Sesame</h1>
-            <h3>Tell us what you're looking for</h3>
         </div>
     );
 }
